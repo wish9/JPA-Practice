7ae5466068169ce64472be6db2a6df66463410d4
wish9 <wjwee9@gmail.com>
2023-02-23 21:21:52 +0900

    1차 캐쉬 + DB에 객체 저장 (영속성 컨텍스트(Persistence Context)에 있는 1차 캐쉬)

--- src/main/java/com/codestates/JpaBasicConfig.java
+++ src/main/java/com/codestates/JpaBasicConfig.java
@@ -12,14 +12,16 @@ import javax.persistence.EntityTransaction;
 @Configuration
 public class JpaBasicConfig {
     private EntityManager em;
+    private EntityTransaction tx;
 
 
     @Bean
     public CommandLineRunner testJpaBasicRunner(EntityManagerFactory emFactory) { // EntityManagerFactory클래스를 이용해 DI 받기
         this.em = emFactory.createEntityManager();
+        this.tx = em.getTransaction();
 
         return args -> {
-            example01(); // Member 엔티티 클래스의 객체를 JPA의 영속성 컨텍스트에 저장
+            example02(); // Member 엔티티 클래스의 객체를 JPA의 영속성 컨텍스트에 저장
         };
     }
 
@@ -31,5 +33,23 @@ public class JpaBasicConfig {
         Member resultMember = em.find(Member.class, 1L);
         System.out.println("Id: " + resultMember.getMemberId() + ", email: " +
                 resultMember.getEmail());
+    }
+
+    private void example02() {
+        tx.begin(); // Transaction을 시작하기 위해서 tx.begin() 메서드를 먼저 호출해야 함
+        Member member = new Member("hgd@gmail.com");
+
+        em.persist(member); // member 객체를 영속성 컨텍스트에 저장
+
+        tx.commit(); // tx.commit()을 호출하는 시점에 영속성 컨텍스트에 저장되어 있는 member 객체를 데이터베이스의 테이블에 저장
+
+        Member resultMember1 = em.find(Member.class, 1L);
+
+        System.out.println("Id: " + resultMember1.getMemberId() + ", email: " + resultMember1.getEmail());
+
+        Member resultMember2 = em.find(Member.class, 2L);
+
+        System.out.println(resultMember2 == null);
+
     }
 }
